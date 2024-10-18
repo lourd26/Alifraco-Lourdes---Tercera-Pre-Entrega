a5e618e17b567f0cfe210e046d5146e5450cab25
Lourdes Alifraco <lourdes.alifraco1998@gmail.com>
2024-10-17 23:52:36 -0300

    Preentrega 3

--- comienzo/urls.py
+++ comienzo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from comienzo.views import comienzo,acerca_de_mi,template1,crear_acolchados
+from comienzo.views import comienzo,acerca_de_mi,template1,crear_acolchados,buscar_acolchados
 
 app_name="comienzo"
 
@@ -7,7 +7,8 @@ urlpatterns = [
    path("",comienzo,name="inicio"),
    path("acerca-de-mi/",acerca_de_mi,name="acerca_de_mi"),
    path("template1/",template1,name="template1"),
-   path("crear-acolchados/<str:tamanio>/<str:color>/<str:composicion>",crear_acolchados,name="crear_acolchados"),
+   path("buscar-acolchados/",buscar_acolchados,name="buscar_acolchados"),
+   path("crear-acolchados/",crear_acolchados,name="crear_acolchados"),
 
     
 ]
--- comienzo/views.py
+++ comienzo/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 from django.template import Template,Context,loader
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from comienzo.models import Acolchados
+from comienzo.forms import CrearFormulario, BuscarFormulario
 
 def comienzo(request):
-    # return HttpResponse("Bienvenidos")
     return render(request,"comienzo/index.html")
 
 def template1(request):
@@ -13,7 +13,23 @@ def template1(request):
 def acerca_de_mi(request):
     return render(request,"comienzo/acerca_de_mi.html")
 
-def crear_acolchados(request,tamanio,color,composicion):
-    acolchados=Acolchados(tamanio=tamanio,color=color,composicion=composicion)
-    acolchados.save()
-    return render(request,"comienzo/creacion_acolchado.html",{"acolchados":acolchados})
+
+def buscar_acolchados(request):
+    formulario=BuscarFormulario(request.GET)
+    if formulario.is_valid():
+        color=formulario.cleaned_data.get("color")
+        composicion=formulario.cleaned_data.get("composicion")
+        acolchados=Acolchados.objects.filter(color__icontains=color,composicion__icontains=composicion)
+    return render(request,"comienzo/buscar_acolchados.html", {"acolchados":acolchados,"form":formulario})
+    
+def crear_acolchados(request):
+    formulario=CrearFormulario()
+    if request.method=="POST":
+        formulario= CrearFormulario(request.POST)
+        if formulario.is_valid():
+            data=formulario.cleaned_data
+            acolchados=Acolchados(tamanio=data.get("tamanio"),color=data.get("color"),composicion=data.get("composicion"))
+            acolchados.save()
+            return redirect("comienzo:crear_acolchados")
+            
+    return render(request,"comienzo/crear_acolchados.html", {"form":formulario})
